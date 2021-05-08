072d6b12d07093a455ae52a6d614a1fbeec6d728
vnz <marshrmt@gmail.com>
2021-05-08 06:45:32 +0300

    init

---
+++ SimpleTrade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExileCore;
+using ExileCore.PoEMemory.Elements;
+using ExileCore.PoEMemory.Components;
+using ExileCore.Shared.Enums;
+using SharpDX;
+
+namespace SimpleTrade
+{
+    class SimpleTrade : BaseSettingsPlugin<SimpleTradeSettings>
+    {
+
+        public override void Render()
+        {
+            Graphics.DrawText("Simple Trade is working", new SharpDX.Vector2(100, 100));
+        }
+    }
+}
---
+++ SimpleTradeSettings.cs
@@ -0,0 +1,11 @@
+﻿using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using ExileCore.Shared.Attributes;
+
+namespace SimpleTrade
+{
+    class SimpleTradeSettings : ISettings
+    {
+        public ToggleNode Enable { get; set; } = new ToggleNode(true);
+    }
+}
