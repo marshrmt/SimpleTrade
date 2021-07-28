6785fc53b55852c0879bb5407f5037db8581d2e2
vnz <marshrmt@gmail.com>
2021-07-28 18:35:39 +0300

    test child count

--- SimpleTrade.cs
+++ SimpleTrade.cs
@@ -248,6 +248,9 @@ namespace SimpleTrade
             Graphics.DrawText("Simple Trade is working", new SharpDX.Vector2(100, 100));
             Graphics.DrawText($"Invites Panel is visible: {GameController.IngameState.IngameUi.InvitesPanel.IsVisible}", new SharpDX.Vector2(100, 120));
             Graphics.DrawText($"Trade Window is visible: {GameController.IngameState.IngameUi.TradeWindow.IsVisible}", new SharpDX.Vector2(100, 140));
+            Graphics.DrawText($"Trade Window is visible: {GameController.IngameState.IngameUi?.TradeWindow?.ChildCount}", new SharpDX.Vector2(100, 180));
+            Graphics.DrawText($"Trade Window is visible: {GameController.IngameState.IngameUi?.InvitesPanel?.ChildCount}", new SharpDX.Vector2(100, 200));
+
 
             if (GameController.IngameState.IngameUi.TradeWindow.IsVisible)
             {
